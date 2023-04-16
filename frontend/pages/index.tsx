b0eb1b5868db8dee0dc921f50b1705745face617
jess-nl <jnoriegalessard@gmail.com>
2023-04-15 21:01:51 -0400

    styles: create header component, wip

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -11,43 +11,12 @@ import {
   AspectRatio,
   Stack,
 } from "@chakra-ui/react";
+import Header from "../components/Header";
 
 const Index = ({ posts }: any) => {
   return (
     <ChakraProvider>
-      <Stack position="absolute" zIndex={1} w="100%">
-        <Flex p="14">
-          <Box bg="red.400">
-            <Text fontSize="110px" lineHeight="110%">
-              Eva <br /> Kabuya
-            </Text>
-          </Box>
-          <Spacer />
-          <VStack textAlign="right" spacing="100px">
-            <Box p="4" bg="green.400" w="360px">
-              <Text fontSize="3xl" lineHeight="80%">
-                About Me
-              </Text>
-              <Text fontSize="2xl">fr | en</Text>
-            </Box>
-            <Box p="4" bg="blue.400" w="360px">
-              <Text fontSize="4xl">Biography</Text>
-              <Text fontSize="4xl">Released Works</Text>
-              <Text fontSize="4xl">Timeline</Text>
-              <Text fontSize="4xl">Work With Me</Text>
-            </Box>
-          </VStack>
-        </Flex>
-      </Stack>
-      <Box bg="tomato" maxW="900px" height="370px" overflow="hidden" mx="auto">
-        <AspectRatio maxW="900px" ratio={16 / 9} mt="-70px">
-          <iframe
-            title="tbd"
-            src="https://player.vimeo.com/video/530984056?h=337e6011fd?autoplay=1&autopause=0&muted=1&background=1#t=0m1s"
-            allowFullScreen
-          />
-        </AspectRatio>
-      </Box>
+      <Header />
       <h1>Welcome to a blog!</h1>
       {posts.length > 0 &&
         posts.map(
