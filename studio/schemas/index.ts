e9a1e3454ae924dad7c4a6bf61e926b65480777a
jess-nl <jnoriegalessard@gmail.com>
2023-04-16 00:49:48 -0400

    feat: add schema for timeline

--- studio/schemas/index.ts
+++ studio/schemas/index.ts
@@ -1,5 +1,6 @@
 import blockContent from './blockContent'
 import category from './category'
 import post from './post'
+import timeline from './timeline'
 
-export const schemaTypes = [post, category, blockContent]
+export const schemaTypes = [post, category, blockContent, timeline]
