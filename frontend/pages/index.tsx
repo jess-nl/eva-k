d2673f5c55431c6b250cc8a9d66cd4b8cf0691c2
jess-nl <jnoriegalessard@gmail.com>
2023-04-15 23:09:27 -0400

    feat: bring blog richtext to homepage

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,16 +2,35 @@ import Link from "next/link";
 import groq from "groq";
 import client from "../client";
 import { ChakraProvider } from "@chakra-ui/react";
-import {
-  Text,
-  Flex,
-  Box,
-  Spacer,
-  VStack,
-  AspectRatio,
-  Stack,
-} from "@chakra-ui/react";
+import imageUrlBuilder from "@sanity/image-url";
+import { PortableText } from "@portabletext/react";
 import Header from "../components/Header";
+
+function urlFor(source: any) {
+  return imageUrlBuilder(client).image(source);
+}
+
+const ptComponents = {
+  types: {
+    image: ({ value }: any) => {
+      if (!value?.asset?._ref) {
+        return null;
+      }
+      return (
+        <img
+          alt={value.alt || " "}
+          loading="lazy"
+          src={urlFor(value)
+            .width(320)
+            .height(240)
+            .fit("max")
+            .auto("format")
+            .url()}
+        />
+      );
+    },
+  },
+};
 
 const Index = ({ posts }: any) => {
   return (
@@ -20,13 +39,14 @@ const Index = ({ posts }: any) => {
       <h1>Welcome to a blog!</h1>
       {posts.length > 0 &&
         posts.map(
-          ({ _id, title = "", slug = "", _updatedAt = "" }: any) =>
+          ({ _id, title = "", slug = "", _updatedAt = "", body = [] }: any) =>
             slug && (
               <li key={_id}>
                 <Link href={`/post/${encodeURIComponent(slug.current)}`}>
                   {title}
                 </Link>{" "}
                 ({new Date(_updatedAt).toDateString()})
+                <PortableText value={body} components={ptComponents} />
               </li>
             )
         )}
