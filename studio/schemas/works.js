16822799b117bfb800308da98604e08ec5f0cca1
jess-nl <jnoriegalessard@gmail.com>
2023-04-16 20:05:07 -0400

    feat: create schema for works

---
+++ studio/schemas/works.js
@@ -0,0 +1,74 @@
+import {defineField, defineType} from 'sanity'
+import Placeholder from '../components/Placeholder'
+
+export default defineType({
+  name: 'works',
+  title: 'Released Works',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    }),
+    defineField({
+      name: 'categories',
+      title: 'Categories',
+      type: 'array',
+      of: [{type: 'reference', to: {type: 'category'}}],
+    }),
+    defineField({
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+    }),
+    defineField({
+      name: 'yearReleased',
+      title: 'Year Released',
+      type: 'date',
+			options: {
+        dateFormat: 'YYYY',
+			}
+    }),
+    defineField({
+      name: 'vimeoUrl',
+      title: 'Vimeo Video URL',
+      type: 'string',
+      components: {
+        input: Placeholder,
+      },
+			options: {
+				placeholder: 'https://player.vimeo.com/video/530984056?h=337e6011fd'
+			}
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'string',
+      options: {
+        maxLength: 75,
+      },
+    }),
+    defineField({
+      name: 'longSummary',
+      title: 'Long Summary',
+      type: 'blockContent',
+    }),
+  ],
+
+  preview: {
+    select: {
+      title: 'title',
+      media: 'vimeoUrl',
+    },
+  },
+})
