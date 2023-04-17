9f5a575f34775150eb45352e48168aca8d028a7f
jess-nl <jnoriegalessard@gmail.com>
2023-04-16 20:11:43 -0400

    feat: add new schema types to index

--- studio/schemas/index.ts
+++ studio/schemas/index.ts
@@ -2,5 +2,9 @@ import blockContent from './blockContent'
 import category from './category'
 import post from './post'
 import timeline from './timeline'
+import header from './header'
+import biography from './biography'
+import works from './works'
+import contact from './contact'
 
-export const schemaTypes = [post, category, blockContent, timeline]
+export const schemaTypes = [post, category, blockContent, header, biography, timeline, works, contact]
