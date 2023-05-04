eacc8c022e070a8d6f401244e0b24420da1e78ca
jess-nl <jnoriegalessard@gmail.com>
2023-05-04 01:45:10 -0400

    feat: create singleton logic

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -3,6 +3,15 @@ import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
+const singletonActions = new Set(['publish', 'discardChanges', 'restore'])
+const singletonTypes = new Set(['header', 'biography', 'contact'])
+
+const singletonListItem = (S: any, typeName: string, title?: string) =>
+  S.listItem()
+    .title(title || typeName)
+    .id(typeName)
+    .child(S.document().schemaType(typeName).documentId(typeName))
+
 export default defineConfig({
   name: 'default',
   title: 'eva-k',
@@ -10,9 +19,31 @@ export default defineConfig({
   projectId: '79m64vwp',
   dataset: 'production',
 
-  plugins: [deskTool(), visionTool()],
+  plugins: [
+    deskTool({
+      structure: (S) =>
+        S.list()
+          .title('Content')
+          .items([
+            singletonListItem(S, 'header', 'Header'),
+            singletonListItem(S, 'biography', 'Biography'),
+            singletonListItem(S, 'contact', 'Work With Me'),
+            S.documentTypeListItem('works').title('Released Works'),
+            S.documentTypeListItem('timeline').title('Timeline'),
+          ]),
+    }),
+    visionTool(),
+  ],
 
   schema: {
     types: schemaTypes,
+    templates: (templates) => templates.filter(({schemaType}) => !singletonTypes.has(schemaType)),
+  },
+
+  document: {
+    actions: (input, context) =>
+      singletonTypes.has(context.schemaType)
+        ? input.filter(({action}) => action && singletonActions.has(action))
+        : input,
   },
 })
