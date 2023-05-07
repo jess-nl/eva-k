33030d2d6d1c9e1f25b3aa86727931ed26c25026
jess-nl <jnoriegalessard@gmail.com>
2023-05-07 00:29:28 -0400

    refactor: remove 'post' schema and slug file

--- studio/schemas/index.ts
+++ studio/schemas/index.ts
@@ -1,10 +1,9 @@
 import blockContent from './blockContent'
 import category from './category'
-import post from './post'
 import timeline from './timeline'
 import header from './header'
 import biography from './biography'
 import works from './works'
 import contact from './contact'
 
-export const schemaTypes = [post, category, blockContent, header, biography, timeline, works, contact]
+export const schemaTypes = [category, blockContent, header, biography, timeline, works, contact]
